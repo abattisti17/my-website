80d55362b809ac5c28bf3fd9d3f8748efaa30d36
Alessandro Battisti <battist@dropbox.com>
2025-08-27 21:26:34 -0400

    feat: remove instructional text from message composer
    - Remove 'Press Enter to send, Shift+Enter for new line' text below compose box
    - Cleaner UI without unnecessary instructional text
    - Users can discover keyboard shortcuts naturally

--- crew-generator/src/components/ui/message-composer.tsx
+++ crew-generator/src/components/ui/message-composer.tsx
@@ -255,12 +255,7 @@ export const MessageComposer: React.FC<MessageComposerProps> = ({
         </IconButton>
         </div>
 
-        {/* Keyboard hint - reduced spacing */}
-        <div className="text-center" style={{ marginTop: 'var(--composer-bottom-spacing)' }}>
-          <p className="text-xs text-muted-foreground">
-            Press Enter to send, Shift+Enter for new line
-          </p>
-        </div>
+
       </div>
     </div>
   )
