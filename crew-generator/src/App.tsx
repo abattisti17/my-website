afca0a9236e9cf4fd6a801ab7f3842a2e333ff3a
Alessandro Battisti <battist@dropbox.com>
2025-08-28 23:02:26 -0400

    fix: remove missing FeatureFlagProvider, CP-A now working
    - Applied CP-A Surgical Debug Checklist
    - Issue was missing FeatureFlagProvider import/context
    - App now renders successfully with IonApp + BrowserRouter
    - All routes functional, no white screen

--- crew-generator/src/App.tsx
+++ crew-generator/src/App.tsx
@@ -1,5 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { IonApp } from '@ionic/react'
+import { ThemeProvider } from 'next-themes'
+import AuthProvider from './components/AuthProvider'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Import pages
 import HomePage from './pages/HomePage'
@@ -16,36 +18,29 @@ import PhotosOverviewPage from './pages/PhotosOverviewPage'
 import MemorabiliPage from './pages/MemorabiliPage'
 import StyleGuidePage from './pages/StyleGuidePage'
 
-// Import providers
-import { AuthProvider } from './components/AuthProvider'
-import { FeatureFlagProvider } from './contexts/FeatureFlagContext'
-import { ThemeProvider } from 'next-themes'
-
 export default function App() {
   return (
     <IonApp>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <FeatureFlagProvider>
-          <AuthProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/auth" element={<AuthPage />} />
-                <Route path="/create-event" element={<CreateEventPage />} />
-                <Route path="/event/:slug" element={<EventPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/profile/edit" element={<ProfileEditPage />} />
-                <Route path="/pod/:podId" element={<PodPage />} />
-                <Route path="/tour/:tourId" element={<TourPage />} />
-                <Route path="/chat" element={<ChatOverviewPage />} />
-                <Route path="/messages" element={<MessagesExamplePage />} />
-                <Route path="/photos" element={<PhotosOverviewPage />} />
-                <Route path="/memorabilia" element={<MemorabiliPage />} />
-                <Route path="/style-guide" element={<StyleGuidePage />} />
-              </Routes>
-            </BrowserRouter>
-          </AuthProvider>
-        </FeatureFlagProvider>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/auth" element={<AuthPage />} />
+              <Route path="/create-event" element={<CreateEventPage />} />
+              <Route path="/event/:slug" element={<EventPage />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/profile/edit" element={<ProfileEditPage />} />
+              <Route path="/pod/:podId" element={<PodPage />} />
+              <Route path="/tour/:tourId" element={<TourPage />} />
+              <Route path="/chat" element={<ChatOverviewPage />} />
+              <Route path="/messages" element={<MessagesExamplePage />} />
+              <Route path="/photos" element={<PhotosOverviewPage />} />
+              <Route path="/memorabilia" element={<MemorabiliPage />} />
+              <Route path="/style-guide" element={<StyleGuidePage />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
       </ThemeProvider>
     </IonApp>
   )
